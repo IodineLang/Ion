45291af42b9c025d575a7fb554d37caa6f146cba
Aurora01 <aurora@int0x10.com>
2015-07-07 18:56:56 +0100

    Refactored to use loglib

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
+if [ $(id -u) = 0 ]; then
+   echo "Do not run this script as root. Aborting..."
+   exit 1
+fi
 sudo cp -r . /usr/lib/iodine-ion/
 mkdir ~/.iodinepackages
 #sudo cp ./ion_env.sh /etc/profile.d/ion.sh
 sudo cp -r ./ion_exec.sh /usr/bin/ion
 sudo chmod a+x /usr/bin/ion
+ion add-repo Default http://iodinelang.github.io/Ion-Default-Repo
